24b6a8baea082e42feab760c791d459aa6509a06
Omar Ibarra <ing.oaio99@gmail.com>
2022-11-30 02:00:57 +0100

    Third commit changed env

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_PORT = process.env.DB_PORT;
+export const DB_HOST = process.env.DB_HOST || "railway";
+export const DB_USER = process.env.DB_USER || "containers-us-west-135.railway.app";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "KcOAGmRtZPWlnk8JYyqq";
+export const DB_DATABASE = process.env.DB_DATABASE || "6981";
+export const DB_PORT = process.env.DB_PORT || "root";
